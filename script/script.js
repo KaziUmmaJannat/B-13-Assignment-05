27765801c95955d74feb44e7ad7763641e443285
Jannat <kaziumme143@gmail.com>
2026-03-07 03:24:55 +0600

    login page push

---
+++ script/script.js
@@ -0,0 +1,15 @@
+
+// function login(){
+//     const userInput = document.getElementById("username").value;
+//     const passInput = document.getElementById("password").value;
+//     const usernames = ["admin"];
+//     const passwords = ["admin123"];
+
+//     for(let i = 0; i < usernames.length; i++){
+//         if(userInput === usernames[i] && passInput === passwords[i]){
+//             alert("Login Successful");
+//             return;
+//         }
+//     }
+//     alert(" Invalid Username or Password");
+// }
